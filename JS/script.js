536ea8fac3647a561e953db3a9aac0ab0fd092ad
Maher Rahman <MaherRahman1@gmail.com>
2018-08-25 23:12:31 -0500

    Updated the website to have responsive images

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,41 @@
-const BTN = document.querySelector(".btn a");
+var extLinks = document.querySelectorAll('a[href^="http"]');
 
-BTN.classList.remove("hide");
-
-function reveal(current) {
-  //  e.preventDefault();
-
-    current.innerHTML == "Check out my Resume!" ? BTN.innerHTML = "Thank you!" : 
-    BTN.innerHTML = "Check out my Resume!";
-
-
+for (var i = 0; i < extLinks.length; i++) {
+	if (!extLinks[i].hasAttribute("target")) {
+		extLinks[i].setAttribute("target", "_blank");
+	}
 }
 
-BTN.addEventListener('click', function(){ reveal(this); }, false);
+/* Making responsive images */
+
+const IMAGES = document.querySelectorAll("img");
+const SIZES = {
+    showcase: "100vw",
+    reason: "(max-width: 799px) 100vw, 372px",
+    feature: "(max-width: 799px) 100vw, 558px",
+    story: "(max-width: 799px) 100vw, 670px",
+};
+
+function makeSrcset(imgSrc) {
+    let markup = [];
+    let width = 400;
+
+    for (let i = 0; i<5; i++) {
+        markup[i] = imgSrc + "-" + width + ".png " + width + "w";
+        width+=400;
+    }
+
+    return markup.join();
+}
+
+for (let i = 0; i<IMAGES.length; i++) {
+    let imgSrc = IMAGES[i].getAttribute("src");
+    imgSrc = imgSrc.slice(0,-8);
+    let srcset = makeSrcset(imgSrc);
+    IMAGES[i].setAttribute("srcset", srcset);
+
+    let type = IMAGES[i].getAttribute("data-type");
+    let sizes = SIZES[type];
+    IMAGES[i].setAttribute("sizes", sizes);
+
+}
